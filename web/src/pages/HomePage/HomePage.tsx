e2570faf116603ead2d4318520370d91926b22cd
Rodrigo Medina <rodrigom@lyft.com>
2023-02-09 00:52:13 -0600

    Added new initial tantric page

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,26 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+import { useState } from 'react'
+import { TantricProfile } from '@todo-esta-bien/numerodon'
 
 const HomePage = () => {
+  const [birthday, setBirthday] = useState<Date>(new Date(Date.now()))
+  const [tantricProfile, setTantricProfile] = useState<TantricProfile | null>(
+    null
+  )
+
+  const onBirthChange = (newBirthday: string) => {
+    const newDate = new Date(newBirthday)
+    setBirthday(newDate)
+    setTantricProfile(
+      new TantricProfile({
+        day: newDate.getUTCDate(),
+        month: newDate.getUTCMonth() + 1,
+        year: newDate.getUTCFullYear(),
+      })
+    )
+  }
+
   return (
     <>
       <MetaTags title="Home" description="Home page" />
@@ -14,6 +33,27 @@ const HomePage = () => {
         My default route is named <code>home</code>, link to me with `
         <Link to={routes.home()}>Home</Link>`
       </p>
+      <p>
+        <label>
+          Birthday:
+          <input
+            type="date"
+            defaultValue={birthday.toISOString().split('T')[0]}
+            onChange={(e) => onBirthChange(e.target.value)}
+            name="birthday"
+          />
+        </label>
+      </p>
+      {tantricProfile && (
+        <div>
+          <h2>Fecha: {birthday.toUTCString()}</h2>
+          <h3>Alma: {tantricProfile.soul}</h3>
+          <h3>Karma: {tantricProfile.karma}</h3>
+          <h3>Regalo Divino: {tantricProfile.divineGift}</h3>
+          <h3>Vida pasada: {tantricProfile.lastLife}</h3>
+          <h3>Camino: {tantricProfile.path}</h3>
+        </div>
+      )}
     </>
   )
 }
