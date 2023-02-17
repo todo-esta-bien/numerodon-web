ff2f0be459d7c26a2eeea77997dc293c904cf551
Rodrigo Medina <rodrigom@lyft.com>
2023-02-17 00:16:05 -0600

    Added Pythagorean profile

---
+++ web/src/components/PythagoreanProfile/PythagoreanProfile.tsx
@@ -0,0 +1,79 @@
+import { PythagoreanProfile as PythagoreanNumProfile } from '@todo-esta-bien/numerodon'
+
+interface IPythagoreanProfile {
+  birthday: Date
+  firstNames: string
+  fatherLastNames: string
+  motherLastNames: string
+}
+
+const PythagoreanProfile = ({
+  birthday,
+  firstNames,
+  fatherLastNames,
+  motherLastNames,
+}: IPythagoreanProfile) => {
+  const pythagoreanProfile = new PythagoreanNumProfile({
+    day: birthday.getUTCDate(),
+    month: birthday.getUTCMonth() + 1,
+    year: birthday.getUTCFullYear(),
+    names: firstNames,
+    fatherLastNames: fatherLastNames,
+    motherLastNames: motherLastNames,
+  })
+
+  const tableRows = [
+    {
+      name: 'Alma',
+      value: pythagoreanProfile.soul,
+    },
+    {
+      name: 'Personalidad',
+      value: pythagoreanProfile.personality,
+    },
+    {
+      name: 'Expresión',
+      value: pythagoreanProfile.expression,
+    },
+    {
+      name: 'Misión Cósmica',
+      value: pythagoreanProfile.cosmicMission,
+    },
+    {
+      name: 'Equilibrio',
+      value: pythagoreanProfile.balance,
+    },
+    {
+      name: 'Fuerza',
+      value: pythagoreanProfile.strength,
+    },
+    {
+      name: 'Iniciación Espiritual',
+      value: pythagoreanProfile.spiritualInitiation,
+    },
+  ]
+
+  return (
+    <div className="prose">
+      <h2>{pythagoreanProfile.completeName}</h2>
+      <table className="table-zebra table table-auto">
+        <thead>
+          <tr>
+            <th>Tipo de Número</th>
+            <th>Vibración</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableRows.map((row) => (
+            <tr key={row.name}>
+              <td>{row.name}</td>
+              <td>{row.value}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default PythagoreanProfile
