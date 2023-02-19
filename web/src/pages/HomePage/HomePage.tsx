c969e36beea0707888e6ed9fc21b76d673d77a75
Rodrigo Medina <rodrigom@lyft.com>
2023-02-19 00:56:23 -0600

    Refactored home page

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -3,11 +3,41 @@ import { useState } from 'react'
 import TantricProfile from 'src/components/TantricProfile/TantricProfile'
 import PythagoreanProfile from 'src/components/PythagoreanProfile/PythagoreanProfile'
 
+type UserData = {
+  firstNames: string | null
+  fatherLastNames: string | null
+  motherLastNames: string | null
+  birthday: Date
+}
+
 const HomePage = () => {
-  const [birthday, setBirthday] = useState<Date>(new Date(Date.now()))
-  const [names, setNames] = useState<string>('')
-  const [fatherLastNames, setFatherLastNames] = useState<string>('')
-  const [motherLastNames, setMotherLastNames] = useState<string>('')
+  const [userData, setUserData] = useState<UserData>({
+    birthday: new Date(Date.now()),
+    firstNames: '',
+    fatherLastNames: '',
+    motherLastNames: '',
+  })
+
+  const inputElements = [
+    {
+      label: 'Nombre (no apellidos)',
+      groupLabel: 'Nombre(s)',
+      placeholder: 'Leticia Verónica',
+      inputName: 'firstNames',
+    },
+    {
+      label: 'Apellidos Paternos',
+      groupLabel: 'Paternos',
+      placeholder: 'Neri',
+      inputName: 'fatherLastNames',
+    },
+    {
+      label: 'Apellidos Maternos',
+      groupLabel: 'Maternos',
+      placeholder: 'Guzmán',
+      inputName: 'motherLastNames',
+    },
+  ]
 
   return (
     <>
@@ -17,58 +47,53 @@ const HomePage = () => {
         <header className="prose">
           <h1>Hola 👋!</h1>
         </header>
-        <p>
-          <label className="text-xl">
-            Escoge tu fecha:
-            <input
-              className="md:ml-4"
-              type="date"
-              defaultValue={birthday.toISOString().split('T')[0]}
-              onChange={(e) => setBirthday(new Date(e.target.value))}
-              name="birthday"
-            />
-          </label>
-        </p>
-        <p>
-          <label className="text-xl">
-            Nombre:
-            <input
-              className="md:ml-4"
-              type="text"
-              onChange={(e) => setNames(e.target.value)}
-              name="name"
-            />
-          </label>
-        </p>
-        <p>
-          <label className="text-xl">
-            Apellidos Paternos:
-            <input
-              className="md:ml-4"
-              type="text"
-              onChange={(e) => setFatherLastNames(e.target.value)}
-              name="name"
-            />
-          </label>
-        </p>
-        <p>
-          <label className="text-xl">
-            Apellidos Maternos:
-            <input
-              className="md:ml-4"
-              type="text"
-              onChange={(e) => setMotherLastNames(e.target.value)}
-              name="name"
-            />
-          </label>
-        </p>
-        <TantricProfile birthday={birthday} />
-        <PythagoreanProfile
-          birthday={birthday}
-          firstNames={names}
-          fatherLastNames={fatherLastNames}
-          motherLastNames={motherLastNames}
-        />
+        <section>
+          <div>
+            <label className="label">
+              <span className="label-text">La fecha de nacimiento</span>
+            </label>
+            <label className="input-group">
+              <span>Fecha</span>
+              <input
+                className="input-bordered input md:ml-4"
+                type="date"
+                defaultValue={userData.birthday.toISOString().split('T')[0]}
+                onChange={(e) =>
+                  setUserData((prevData: UserData) => ({
+                    ...prevData,
+                    birthday: new Date(e.target.value),
+                  }))
+                }
+                name="birthday"
+              />
+            </label>
+          </div>
+          {inputElements.map((element) => (
+            <div>
+              <label className="label">
+                <span className="label-text">{element.label}</span>
+              </label>
+              <label className="input-group">
+                <span>{element.groupLabel}</span>
+                <input
+                  className="input-bordered input md:ml-4"
+                  type="text"
+                  onChange={(e) =>
+                    setUserData((prevData: UserData) => ({
+                      ...prevData,
+                      [element.inputName]: e.target.value,
+                    }))
+                  }
+                  name={element.inputName}
+                  placeholder={element.placeholder}
+                />
+              </label>
+            </div>
+          ))}
+        </section>
+
+        <TantricProfile birthday={userData.birthday} />
+        <PythagoreanProfile {...userData} />
       </main>
     </>
   )
