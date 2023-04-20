3f07f0106bb03e0927f18974de63eb9eb7728b58
Rodrigo Medina <rodrigom@lyft.com>
2023-04-19 20:54:59 -0600

    Added 1st version of the Diagram

---
+++ web/src/components/Base22Diagram/Base22Diagram.stories.tsx
@@ -0,0 +1,25 @@
+// When you've added props to your component,
+// pass Storybook's `args` through this story to control it from the addons panel:
+//
+// ```tsx
+// import type { ComponentStory } from '@storybook/react'
+//
+// export const generated: ComponentStory<typeof Base22Diagram> = (args) => {
+//   return <Base22Diagram {...args} />
+// }
+// ```
+//
+// See https://storybook.js.org/docs/react/writing-stories/args.
+
+import type { ComponentMeta } from '@storybook/react'
+
+import Base22Diagram from './Base22Diagram'
+
+export const generated = () => {
+  return <Base22Diagram />
+}
+
+export default {
+  title: 'Components/Base22Diagram',
+  component: Base22Diagram,
+} as ComponentMeta<typeof Base22Diagram>
---
+++ web/src/components/Base22Diagram/Base22Diagram.test.tsx
@@ -0,0 +1,14 @@
+import { render } from '@redwoodjs/testing/web'
+
+import Base22Diagram from './Base22Diagram'
+
+//   Improve this test with help from the Redwood Testing Doc:
+//    https://redwoodjs.com/docs/testing#testing-components
+
+describe('Base22Diagram', () => {
+  it('renders successfully', () => {
+    expect(() => {
+      render(<Base22Diagram />)
+    }).not.toThrow()
+  })
+})
