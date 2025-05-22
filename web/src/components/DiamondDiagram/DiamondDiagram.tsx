66c3198e36f92c8b1f4941f197a817ccbf458353
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2025-05-22 11:26:12 -0600

    centered the R number

--- web/src/components/DiamondDiagram/DiamondDiagram.tsx
+++ web/src/components/DiamondDiagram/DiamondDiagram.tsx
@@ -95,7 +95,7 @@ const DiamondDiagram = ({ birthday }: IDiamondDiagram) => {
           <text transform="matrix(1 0 0 1 325.4462 239.8129)" className="diamond-st2 st5">
             {displayReducedNumber(diamondProfile.starFacet)}
           </text>
-          <text transform="matrix(1 0 0 1 311.7498 297.8519)" className="diamond-st2 st5">
+          <text transform="matrix(1 0 0 1 330.52 297.8519)" className="diamond-st2 st5" textAnchor="middle">
             {displayReducedNumber(diamondProfile.reflect)}
           </text>
           <text transform="matrix(1 0 0 1 406.8838 355.7993)" className="diamond-st2 st5">
